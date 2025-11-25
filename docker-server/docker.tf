9684fed8d5b18cefc64e9eddd8af174d379709d5
rajuvenkatesh <venkateshamsonnalia143gmail.com>
2025-11-25 17:57:25 +0530

    added files

--- docker-server/docker.tf
+++ docker-server/docker.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "docker" {
   ami             = var.ami_id
   instance_type   = var.instance_type
   security_groups = [aws_security_group.sg.name]
-  key_name = "docker"
-  user_data = file("docker.sh")
+  key_name        = "docker"
+  user_data       = file("docker.sh")
   tags = {
     Name         = "Docker"
     Terraform    = "true"
